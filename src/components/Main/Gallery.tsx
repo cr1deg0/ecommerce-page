c31e27bf7efe92eccaa8a18a3acddf9bf14dfbc8
cr1deg0 <crisidego@gmail.com>
2023-02-28 17:06:01 +0000

    implement gallery button actions mobile design

--- src/components/Main/Gallery.tsx
+++ src/components/Main/Gallery.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { useSmallScreen } from '../../hooks/useSmallScreen'
 import './styles/Gallery.scss'
 
 const productImages = [
@@ -24,49 +26,69 @@ const productImages = [
 ]
 
 const Gallery = () => {
+	const smallScreen = useSmallScreen()
+	const [index, setIndex] = useState(0)
+
+	const nextIndex = () => {
+		index < productImages.length - 1
+			? setIndex((prev) => prev + 1)
+			: setIndex(0)
+	}
+	const prevIndex = () => {
+		index > 0
+			? setIndex((prev) => prev - 1)
+			: setIndex(productImages.length - 1)
+	}
+	// console.log(productImages.length)
 	return (
 		<div className='gallery'>
-			<button className='btn-previous'>
-				<svg
-					width='12'
-					height='18'
-					xmlns='http://www.w3.org/2000/svg'
-					aria-role='image'
-					aria-labelledby='btn-img-previous'
-				>
-					<title id='btn-img-previous'>Left pointing arrow</title>
-					<path
-						d='M11 1 3 9l8 8'
-						stroke='#1D2026'
-						stroke-width='3'
-						fill='none'
-						fillRule='evenodd'
-					/>
-				</svg>
-			</button>
+			{smallScreen && (
+				<button className='btn-previous'>
+					<svg
+						width='12'
+						height='18'
+						xmlns='http://www.w3.org/2000/svg'
+						role='image'
+						aria-labelledby='btn-img-previous'
+						onClick={() => prevIndex()}
+					>
+						<title id='btn-img-previous'>Left pointing arrow</title>
+						<path
+							d='M11 1 3 9l8 8'
+							stroke='#1D2026'
+							stroke-width='3'
+							fill='none'
+							fillRule='evenodd'
+						/>
+					</svg>
+				</button>
+			)}
 			<img
-				src={productImages[0].src}
-				alt={productImages[0].alt}
+				src={productImages[index].src}
+				alt={productImages[index].alt}
 				className='main-img'
 			/>
-			<button className='btn-next'>
-				<svg
-					width='13'
-					height='18'
-					xmlns='http://www.w3.org/2000/svg'
-					aria-role='image'
-					aria-labelledby='btn-img-next'
-				>
-					<title id='btn-img-next'>Right pointing arrow</title>
-					<path
-						d='m2 1 8 8-8 8'
-						stroke='#1D2026'
-						stroke-width='3'
-						fill='none'
-						fillRule='evenodd'
-					/>
-				</svg>
-			</button>
+			{smallScreen && (
+				<button className='btn-next'>
+					<svg
+						width='13'
+						height='18'
+						xmlns='http://www.w3.org/2000/svg'
+						role='image'
+						aria-labelledby='btn-img-next'
+						onClick={() => nextIndex()}
+					>
+						<title id='btn-img-next'>Right pointing arrow</title>
+						<path
+							d='m2 1 8 8-8 8'
+							stroke='#1D2026'
+							stroke-width='3'
+							fill='none'
+							fillRule='evenodd'
+						/>
+					</svg>
+				</button>
+			)}
 			<div className='gallery--thumbnail'>
 				{productImages.map((item) => (
 					<button key={item.id} className='thumbnail-btn'>
