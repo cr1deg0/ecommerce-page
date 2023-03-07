04f0391536ed306d0cd17ae9893e7e94118561c3
cr1deg0 <crisidego@gmail.com>
2023-03-07 12:56:24 +0000

    added screen size condition to lightbox rendering

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import './globalStyles/base.scss'
 import Header from './components/Header/main'
 import Main from './components/Main/main'
 import Lightbox from './components/Lightbox/Lightbox'
+import { useSmallScreen } from './hooks/useSmallScreen'
 
 function App() {
 	const [toggleLightbox, setToggleLightbox] = useState(false)
+	const smallScreen = useSmallScreen()
 
 	return (
 		<LightboxToggleContext.Provider
 			value={{ toggleLightbox, setToggleLightbox }}
 		>
-			{toggleLightbox && <Lightbox />}
+			{toggleLightbox && !smallScreen && <Lightbox />}
 			<Header />
 			<Main />
 		</LightboxToggleContext.Provider>
