94b4d99b38513d55f29ab03a08627285d02e77a1
cr1deg0 <crisidego@gmail.com>
2023-03-13 14:15:17 +0000

    initial cart component

---
+++ src/components/Cart/Cart.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles/Cart.scss'
+
+const Cart = () => {
+	return (
+		<div className='cart'>
+			<h3>Cart</h3>
+			<div>
+				<p>Your cart is empty.</p>
+				{/* CartItem */}
+				{/* Checkout button */}
+			</div>
+		</div>
+	)
+}
+
+export default Cart
--- src/components/Header/main.tsx
+++ src/components/Header/main.tsx
@@ -3,6 +3,7 @@ import { useSmallScreen } from '../../hooks/useSmallScreen'
 import './styles/main.scss'
 import NavBar from './NavBar'
 import NavToggler from './NavToggler'
+import Cart from '../Cart/Cart'
 
 const Header = () => {
 	const smallScreen = useSmallScreen()
@@ -16,34 +17,37 @@ const Header = () => {
 		}
 	}, [smallScreen])
 	return (
-		<header aria-label='Main menu'>
-			<a className='sr-only' href=''>
-				Skip to main content
-			</a>
-			<div className='header'>
-				<div className='header--left'>
-					{smallScreen && (
-						<NavToggler
-							menuOpen={menuOpen}
-							setMenuOpen={setMenuOpen}
-						/>
-					)}
-					<img src='./images/logo.svg' alt='Sneakers' />
-					<NavBar menuOpen={menuOpen} />
+		<>
+			<header aria-label='Main menu'>
+				<a className='sr-only' href=''>
+					Skip to main content
+				</a>
+				<div className='header'>
+					<div className='header--left'>
+						{smallScreen && (
+							<NavToggler
+								menuOpen={menuOpen}
+								setMenuOpen={setMenuOpen}
+							/>
+						)}
+						<img src='./images/logo.svg' alt='Sneakers' />
+						<NavBar menuOpen={menuOpen} />
+					</div>
+					<div className='header--right'>
+						<button>
+							<img src='./images/icon-cart.svg' alt='Shopping cart' />
+						</button>
+						<a href='/#' aria-label='Access your account data'>
+							<img
+								src='./images/image-avatar.png'
+								alt='your profile picture'
+							/>
+						</a>
+					</div>
 				</div>
-				<div className='header--right'>
-					<button>
-						<img src='./images/icon-cart.svg' alt='Shopping cart' />
-					</button>
-					<a href='/#' aria-label='Access your account data'>
-						<img
-							src='./images/image-avatar.png'
-							alt='your profile picture'
-						/>
-					</a>
-				</div>
-			</div>
-		</header>
+			</header>
+			<Cart />
+		</>
 	)
 }
 
