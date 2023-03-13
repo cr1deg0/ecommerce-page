7d0aee6f6c950b5ab854d6b8e4478cf33404105d
cr1deg0 <crisidego@gmail.com>
2023-03-13 17:12:02 +0000

    new cart item component and overal cart styling

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import './styles/Cart.scss'
+import CartItem from '../CartItem/CartItem'
 
 const Cart = () => {
 	return (
 		<div className='cart'>
-			<h3>Cart</h3>
-			<div>
-				<p>Your cart is empty.</p>
-				{/* CartItem */}
-				{/* Checkout button */}
+			<div className='cart-header'>
+				<h3>Cart</h3>
+				<button>
+					<svg
+						width='14'
+						height='15'
+						xmlns='http://www.w3.org/2000/svg'
+						role='img'
+						aria-label='Close menu icon'
+					>
+						<path
+							d='m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z'
+							fill-rule='evenodd'
+						/>
+					</svg>
+				</button>
+			</div>
+			<div className='cart-container'>
+				{/* <p className='cart-empty'>Your cart is empty.</p> */}
+				<CartItem />
+				<button className='cart-checkout'>Checkout</button>
 			</div>
 		</div>
 	)
--- src/data/productData.tsx
+++ src/data/productData.tsx
@@ -6,6 +6,7 @@ const productData = [
 			"These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
 		price: '250.00',
 		discount: '50',
+		discounted_price: '125.00',
 		images: [
 			{
 				id: 0,
