5408f78cac8f35bd48f5513e4dfe0c934992807f
cr1deg0 <crisidego@gmail.com>
2023-03-17 15:17:22 +0000

    remove unused import

--- src/components/Cart/__tests__/Cart.test.tsx
+++ src/components/Cart/__tests__/Cart.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen, waitFor } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import { describe, it, vi } from 'vitest'
 import { ProductContext } from '../../../context/ProductProvider'
 import { CartContext } from '../../../context/CartProvider'
